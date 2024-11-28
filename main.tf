23ff828cb355414ec4d61202c8d26ecb18f19318
Leonardo <rmnobarra@gmail.com>
2024-11-28 14:32:33 -0300

    feat: add new local file resource.

--- main.tf
+++ main.tf
@@ -1,38 +1,38 @@
-# Generate a random string
-resource "random_string" "random_text" {
-  length  = 16
-  special = false
-  upper   = false
-}
-
-# Create a local file with the random string
-resource "local_file" "random_file" {
-  filename = "random_text.txt"
-  content  = random_string.random_text.result
-}
-
-# Generate a random string
+# Generate the first random string
 resource "random_string" "random_text" {
   length = 16
   special = false
   upper = false
 }
 
-# Create a local file with the random string
+# Create a local file with the first random string
 resource "local_file" "random_file" {
   filename = "random_text.txt"
-  content  = random_string.random_text.result
+  content = random_string.random_text.result
 }
 
-# Generate another random string
+# Generate the second random string
 resource "random_string" "another_random_text" {
   length = 16
   special = true
   upper = true
 }
 
-# Create another local file with the new random string
+# Create another local file with the second random string
 resource "local_file" "another_random_file" {
   filename = "another_random_text.txt"
-  content  = random_string.another_random_text.result
+  content = random_string.another_random_text.result
+}
+
+# Generate the third random string
+resource "random_string" "third_random_text" {
+  length = 20
+  special = true
+  upper = false
+}
+
+# Create a local file with the third random string
+resource "local_file" "third_random_file" {
+  filename = "third_random_text.txt"
+  content = random_string.third_random_text.result
 }
