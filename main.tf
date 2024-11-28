f1b78cca0331f1006bd180bb10dfd48cc0de455d
Leonardo <rmnobarra@gmail.com>
2024-11-28 14:23:50 -0300

    feat: add another resource block to generate new file

--- main.tf
+++ main.tf
@@ -10,3 +10,29 @@ resource "local_file" "random_file" {
   filename = "random_text.txt"
   content  = random_string.random_text.result
 }
+
+# Generate a random string
+resource "random_string" "random_text" {
+  length = 16
+  special = false
+  upper = false
+}
+
+# Create a local file with the random string
+resource "local_file" "random_file" {
+  filename = "random_text.txt"
+  content  = random_string.random_text.result
+}
+
+# Generate another random string
+resource "random_string" "another_random_text" {
+  length = 16
+  special = true
+  upper = true
+}
+
+# Create another local file with the new random string
+resource "local_file" "another_random_file" {
+  filename = "another_random_text.txt"
+  content  = random_string.another_random_text.result
+}
